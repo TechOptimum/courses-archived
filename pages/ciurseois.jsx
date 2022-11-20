a0756528f6ffa51d399de0511b39c75223350955
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-11-19 22:32:16 -0700

    stuff

---
+++ pages/ciurseois.jsx
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import Link from "next/link";
+import { getDatabase } from "../lib/notion";
+import { Text } from "./[id].js";
+
+
+
+export const databaseId = process.env.NOTION_DATABASE_ID;
+
+export default function miurseois({ posts }) {
+  return (
+   <>
+
+    <div className="announcements-container">
+      
+        <ol className="announcements">
+          {posts.map((post) => {
+            const date = new Date(post.last_edited_time).toLocaleString(
+              "en-US",
+              {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              }
+            );
+            return (
+              
+              <li  className="post"key={post.id} >
+                <h3 className="postTitle">
+                  <Link href={`/${post.id}`}>
+                    <Text text={post.properties.Name.title} />
+                  </Link>
+                </h3>
+
+                <p className="postDescription">{date}</p>
+                <Link className="readMoreLink" href={`/${post.id}`}>Read more → </Link>
+              </li>
+            );
+          })}
+        </ol>
+     
+    </div>
+
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
+
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  
+  };
+};
