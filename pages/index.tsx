524f9cb4ece1e0ce087971f48de6851d3be8fdb1
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-11-20 22:01:23 -0700

    format ++

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+
 const Home: NextPage = () => {
+  
   return (
+    
     <div className={styles.container}>
       <Head>
         <title>Tech Optimum Courses</title>
@@ -15,26 +18,34 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Image
-          width={"400px"}
-          height="150px"
-          src="/text-lblue-transparent.png"
-        />
-        <h1 className={styles.title}>Courses Portal</h1>
-        <br />
-        <br />
 
-        <div className={styles.grid}>
-          <a
-            href="./5b245c12-e448-43ff-8183-373c37db2a62"
-            className={styles.card}
-          >
-            <h2>Intro to Web Dev. &rarr;</h2>
-          </a>
-        </div>
-        <p className="report-bugs-home"> Report course errors to our <a target={"_blank"} href="https://discord.gg/w7cymgdRBH">Discord Server</a></p>
-      </main>
+
+        <main className={styles.main}>
+          <Image
+            width={"400px"}
+            height="150px"
+            src="/text-lblue-transparent.png"
+          />
+          <h1 className={styles.title}>Courses Portal</h1>
+          <br />
+          <br />
+
+          <div className={styles.grid}>
+            <a
+              href="./5b245c12-e448-43ff-8183-373c37db2a62"
+              className={styles.card}
+            >
+              <h2>Intro to Web Dev. &rarr;</h2>
+            </a>
+          </div>
+          <p className="report-bugs-home">
+            {" "}
+            Report course errors to our{" "}
+            <a target={"_blank"} href="https://discord.gg/w7cymgdRBH">
+              Discord Server
+            </a>
+          </p>
+        </main>
     </div>
   );
 };
