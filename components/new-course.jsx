1910b843b2761d285377a3f51124311c56ae0a46
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2022-11-21 17:20:45 -0700

    home page redesign

---
+++ components/new-course.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { BsArrowDownRightCircle } from 'react-icons/bs';
+export default function Course({ homeid, bannerurl, alt, title, description }) {
+  return (
+    <>
+      <div className="course-comp">
+        <Image
+          className="course-image"
+          width={"400px"}
+          height="150px"
+          alt={alt}
+          src={bannerurl}
+        ></Image>
+        <div className="course-cont">
+          <h1>{title}</h1>
+          <div className="flexbox-2">
+         <p>{description}</p>
+          <a className="coursen-button" href={homeid}>Begin Course</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,54 +2,53 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import Course from "../components/new-course.jsx";
 
 const Home: NextPage = () => {
-  
   return (
-    
     <div className={styles.container}>
       <Head>
         <title>Tech Optimum Courses</title>
-        <meta
-          name="description"
-          content="Tech Optimum Courses by Tech Optimum"
-        />
+        <meta name="description" content="Courses by Tech Optimum" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.main}>
+        <Image
+          width={"400px"}
+          height="150px"
+          src="/text-lblue-transparent.png"
+        />
+        <h1 className={styles.title}>Courses Portal</h1>
+        <br />
+        <br />
 
-
-
-        <main className={styles.main}>
-          <Image
-            width={"400px"}
-            height="150px"
-            src="/text-lblue-transparent.png"
-          />
-          <h1 className={styles.title}>Courses Portal</h1>
-          <br />
-          <br />
-
-          <div className={styles.grid}>
-            <a
-              href="./5b245c12-e448-43ff-8183-373c37db2a62"
-              className={styles.card}
-            >
-              <h2>Intro to Web Dev. &rarr;</h2>
-            </a>
-           
-          </div>
+        <div className={styles.grid}>
+          <Course
+            homeid="5b245c12-e448-43ff-8183-373c37db2a62"
+            description="Learn the Basics of HTML, CSS, and JavaScript."
+            title={"Intro to Web Dev."}
+            alt={"Web Developer Banner"}
+            bannerurl={
+              "/Screen Shot 2022-11-21 at 5.20.08 PM.png"
+            }
+          ></Course>
           
-          <p className="report-bugs-home">
-            Return to our <a href="https://techoptimum.org">Main Website</a>
-            {" "}{" "} {" "}
-            <br/><br/>
-            Report course errors to our{" "}
-            <a rel="noreferrer"target={"_blank"} href="https://discord.gg/w7cymgdRBH">
-              Discord Server
-            </a>
-          </p>
-        </main>
+        </div>
+
+        <p className="report-bugs-home">
+          Return to our <a href="https://techoptimum.org">Main Website</a>
+          <br />
+          <br />
+          Report course errors to our{" "}
+          <a
+            rel="noreferrer"
+            target={"_blank"}
+            href="https://discord.gg/w7cymgdRBH"
+          >
+            Discord Server
+          </a>
+        </p>
+      </main>
     </div>
   );
 };
