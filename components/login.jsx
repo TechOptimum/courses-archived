5f1a925b5e5b577e7d30be7e604249a350311550
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-01-12 22:24:26 -0700

    ui revamp

--- components/login.jsx
+++ components/login.jsx
@@ -1,22 +1,49 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Course from "../components/new-course";
+import styles from "../styles/Home.module.css";
 export default function Login() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
-        <button className="button-center" onClick={() => signOut()}>Sign out</button>
-        <button className="button-center" onClick={() => window.location("/", "_self")}>
-          Go to Courses?
-        </button>
+        <div className="center-section">
+          <br />
+
+          <div className="flex">
+            <img className="discord-pfp" width={"40px"} src={session.user.image} />
+            <h1 className="align-center">
+              {session.user.name}, Welcome to Tech Optimum Courses{" "}
+            </h1>
+          </div>
+      
+        </div>
+        <div className={styles.grid}>
+          {/*   <Course
+            homeid="5b245c12-e448-43ff-8183-373c37db2a62"
+            description="Learn the Basics of HTML, CSS, and JavaScript."
+            title={"Intro to Web Dev."}
+            alt={"Web Developer Banner"}
+            bannerurl={"/html.png"}
+  ></Course>*/}
+
+          <Course
+            homeid="32778a40-8f63-4dbc-a6e8-a3f8b6d79e11"
+            description="Learn the basics of Python and learn how to use datasets and variables."
+            title={"Intro to Python"}
+            alt={"Python bingbong"}
+            bannerurl={"/python.jpg"}
+          />
+        </div>
       </>
     );
   }
   return (
     <>
-    <div className="center-div">
-<h1 className="align-center">Please sign in with Discord</h1>
-      <button className="button-center" onClick={() => signIn()}>Sign in</button>
+      <div className="center-div">
+        <h1 className="align-center">Please sign in with Discord</h1>
+        <button className="button-center" onClick={() => signIn()}>
+          Sign in
+        </button>
       </div>
     </>
   );
